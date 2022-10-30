c9609464d372dda703b00aaca8820c9aa1cdfae7
Roberta de Siqueira <designer.roberta@gmail.com>
2022-10-30 17:35:50 -0300

    manipulação de respostas http

---
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,68 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.infrastructure.repository.CozinhaRepositoryImpl;
+
+@RestController
+@RequestMapping(value = "/cozinhas") //, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+public class CozinhaController {
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping
+	public List<Cozinha> listar() {
+		return cozinhaRepository.todas();
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXml() {
+		return new CozinhasXmlWrapper(cozinhaRepository.todas());
+	}
+	
+	@GetMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
+		Cozinha cozinha = cozinhaRepository.porId(id);
+		
+		if (cozinha != null) {
+			//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+			return ResponseEntity.ok(cozinha);
+		}
+		
+		return ResponseEntity.notFound().build();
+		
+//		*** Manipulando Respostas HTTP:		
+//		HttpHeaders headers = new HttpHeaders();
+//		headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+//		
+//		return ResponseEntity
+//				.status(HttpStatus.FOUND)
+//				.headers(headers)
+//				.build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Cozinha> adicionar(@RequestBody Cozinha cozinhaRequest) {
+		Cozinha cozinha = cozinhaRepository.adicionar(cozinhaRequest);
+		return ResponseEntity.ok(cozinha);
+	}
+
+}
